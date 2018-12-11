b8a36281c941e51f29a8c42db06683cb1fca8dfc
Shawn Lin <shawn.lin@garmin.com>
2018-12-11 17:35:31 +0800

    Try to fix requirejs for other html

---
+++ js/home.js
@@ -0,0 +1,9 @@
+define('home', ['vendor/jquery-2.1.4.min',
+      'vendor/extensions/revolution.extension.slideanims.min',
+      'vendor/extensions/revolution.extension.actions.min',
+      'vendor/extensions/revolution.extension.layeranimation.min',
+      'vendor/extensions/revolution.extension.kenburn.min',
+      'vendor/extensions/revolution.extension.navigation.min',
+      'vendor/extensions/revolution.extension.parallax.min'], function(){
+
+});
